b8c0dc3e6b419b7a252879f5a4c8dcb05e073e7b
bargest <bulgakovbarg@gmail.com>
2019-03-19 23:48:56 +0700

    Fix and modules

--- Source/ExampleGame/ExampleGameGameMode.cpp
+++ Source/ExampleGame/ExampleGameGameMode.cpp
@@ -1,7 +1,6 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 #include "ExampleGameGameMode.h"
-#include "ExampleGameCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AExampleGameGameMode::AExampleGameGameMode()
